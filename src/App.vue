b9a4c9cba5e466104a58cf348800f036668b6cdb
alirezaahmadi1308 <alirezaahmadi1308@gmail.com>
2022-07-07 15:24:22 +0430

    create chart vue.js

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ag-charts-vue
+    :options="options"
+    >
+
+
+
+    </ag-charts-vue>
+  </div>
+</template>
+
+
+<script>
+import  {AgChartsVue} from 'ag-charts-vue3'
+
+export default {
+  components:{
+    'ag-charts-vue':AgChartsVue
+  },
+  data:() =>({
+        options:null
+  }),
+  created() {
+    this.options={
+      data: [
+        { os: 'Android', share: 56.9, satisfaction: 10 },
+        { os: 'iOS', share: 22.5, satisfaction: 12 },
+        { os: 'BlackBerry', share: 6.8, satisfaction: 9 },
+        { os: 'Symbian', share: 8.5, satisfaction: 8 },
+        { os: 'Bada', share: 2.6, satisfaction: 7 },
+        { os: 'Windows', share: 1.9, satisfaction: 6 },
+      ],
+      series: [
+        {
+          type: 'pie',
+          labelKey: 'os',
+          angleKey: 'share',
+          radiusKey: 'satisfaction',
+        },
+      ],
+    }
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
+
+
+<style lang="scss">
+
+
+</style>
